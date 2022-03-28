9da496c869173839714e1437c8353a792485dd44
Gustavo Fernandes <gustavomarcelinofernandes25@gmail.com>
2022-03-28 09:02:18 -0300

    Api de login

--- login/index.php
+++ login/index.php
@@ -1,37 +1,35 @@
 <?php
 $login = isset($_GET['usuario']) ? $_GET['usuario'] : null;
-$senha = isset($_GET['senha']) ? $_GET['senha'] : null;
+$senha = isset($_GET['senha']) ? md5($_GET['senha']) : null;
 
+function validaLogin($login, $senha, $con)
+{
+    $return = [];
+    $token = '';
+    $sqlVerificaUsuario = "select idusuario, nome, email, login from usuario where login = '{$login}' and senha = '{$senha}'";
+    $respVerificaUsuario = mysqli_query($con, $sqlVerificaUsuario);
+    $validaUsuario = mysqli_fetch_array($respVerificaUsuario);
+    if (isset($validaUsuario[0])) {
+        $data = ['id' => $validaUsuario[0], 'nome' => $validaUsuario[1], 'email' => $validaUsuario[2], 'login' => $validaUsuario[3], 'token' => $token];
+        return ['status' => true, 'data' => $data];
+    } else {
+        $return = ['status' => false, 'error' => 'Usuário não encontrado.'];
+    }
+    return $return;
+}
 
 if (isset($login) && isset($senha)) {
-    include './app/config/conMysql.php';
+    include '../app/config/conMysql.php';
     $response = [];
-    function validaLogin($login, $senha, $con)
-    {
-        $return = [];
-        $token = '';
-        $codSenha = md5($senha);
-        $sqlVerificaUsuario = "select idusuario, nome, email, login from usuario where login = '{$login}' && senha = '${$codSenha}'";
-        $respVerificaUsuario = mysqli_query($con, $sqlVerificaUsuario);
-        $validaUsuario = mysqli_fetch_array($respVerificaUsuario);
-        if (isset($validaUsuario[0])) {
-            $data = ['id' => $validaUsuario[0], 'nome' => $validaUsuario[1], 'email' => $validaUsuario[2], 'login' => $validaUsuario[3], 'token' => $token];
-            return ['status' => true, 'data' => $data];
-        } else {
-            $return = ['status' => false, 'error' => 'Usuário não encontrado.'];
-        }
-        return $return;
-    }
+
 
     $response = validaLogin($login, $senha, $con);
-
-
-
+    
     if (count($response) > 0) {
         echo json_encode($response);
     } else {
         echo json_encode([]);
     }
 } else {
-    echo 'sem dados';
+    echo 'sem dadosssss';
 }
