44ba350bb778fc99d9138c05bd721a00302cbea7
Gustavo Fernandes <gustavomarcelinofernandes25@gmail.com>
2022-03-28 07:55:14 -0300

    criando retorno da mensagem de login

--- login/index.php
+++ login/index.php
@@ -9,13 +9,21 @@ if (isset($login) && isset($senha)) {
 
     function validaLogin($login, $senha, $con)
     {
+        $return = [];
+        $token = '';
         $codSenha = md5($senha);
         $sqlVerificaUsuario = "select idusuario, nome, email, login from usuario where login = '{$login}' && senha = '${$codSenha}'";
         $respVerificaUsuario = mysqli_query($con, $sqlVerificaUsuario);
         $validaUsuario = mysqli_fetch_array($respVerificaUsuario);
-        print_r($validaUsuario);
+        if (isset($validaUsuario[0])) {
+            $data = ['id' => $validaUsuario[0], 'nome' => $validaUsuario[1], 'email' => $validaUsuario[2], 'login' => $validaUsuario[3],'token' => $token];
+            return ['status' => true, 'data' => $data];
+        } else {
+            $return = ['status' => false, 'error' => 'Usuário não encontrado.'];
+        }
+        return $return;
     }
 
     $response = validaLogin($login, $senha, $con);
-    // print_r($response);
+    echo json_encode($response);
 }
