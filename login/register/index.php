49321db33a550b57bec0165598a57edff6339e33
HerrisonTr <herrison.trugilho2@gmail.com>
2022-04-04 13:29:45 -0300

    Instalando composer junto com a depedência phpMailer

--- login/register/index.php
+++ login/register/index.php
@@ -10,18 +10,27 @@
 //state
 //city
 //emailVerified
-echo "CONEXÃO OK";
+echo "CONEXÃO OK!";
 die();
+
+require '../../vendor/autoload.php';
+include '../../app/config/conMysql.php';
+include 'FuncSendEmail.php';
+
 $login = isset($_GET['login']) ? $_GET['login'] : null;
 $password = isset($_GET['password']) ? $_GET['password'] : null;
 $name = isset($_GET['name']) ? $_GET['name'] : null;
 $email = isset($_GET['email']) ? $_GET['email'] : null;
-//$login = isset($_GET['photo']) ? $_GET['photo'] : null;
+$photo = isset($_GET['photo']) ? $_GET['photo'] : null;
+$datebirth = isset($_GET['datebirth']) ? $_GET['datebirth'] : null;
 $country = isset($_GET['country']) ? $_GET['country'] : null;
 $state = isset($_GET['state']) ? $_GET['state'] : null;
 $city = isset($_GET['city']) ? $_GET['city'] : null;
 
-function register($login, $password, $name, $email, $country, $state, $city, $con)
+$namePhoto = '';
+$hashed_password = password_hash($password, PASSWORD_DEFAULT); //password encryption
+
+function register($login, $hashed_password, $name, $email, $namePhoto, $datebirth, $country, $state, $city, $con)
 {
     $return = [];
     $token = '';
@@ -30,18 +39,35 @@ function register($login, $password, $name, $email, $country, $state, $city, $co
     $resultVerifyUser = mysqli_query($con, $sqlVerifyUser);
     if(mysqli_num_rows($resultVerifyUser) > 0){
         $return = ['status' => false, 'error' => 'Login em uso.'];
-    }
+    } else{
+        $sqlRegisterUser = "insert into user values (null,
+                                                     '{$login}',
+                                                     '{$hashed_password}',   
+                                                     '{$name}',   
+                                                     '{$email}', 
+                                                     '{$namePhoto}',
+                                                     '{$datebirth}',
+                                                     '{$country}',
+                                                     '{$state}',
+                                                     '{$city}',
+                                                     'n')";
+        if(mysqli_query($con, $sqlRegisterUser)){
+            
 
+
+        }                                                     
+    }
     return $return;
 }
 
 if (
     isset($login) && isset($password) &&
     isset($name) && isset($email) &&
-    isset($country) && isset($state) && isset($city)
+    isset($country) && isset($state) &&
+    isset($city) && isset($datebirth)
 ) {
     include '../../app/config/conMysql.php';
     $response = [];
 } else {
-    echo json_encode(['status' => false, 'error' => 'Digite um usuário e senha.']);
+    echo json_encode(['status' => false, 'error' => 'Preencha todos os campos.']);
 }
