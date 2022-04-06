df8045869a6e004eaa6b3a599449369ea6dd833e
Gustavo Fernandes <gustavomarcelinofernandes25@gmail.com>
2022-04-06 09:55:17 -0300

    check email token

--- login/forgetpassword/verifica.php
+++ login/forgetpassword/verifica.php
@@ -1,10 +1,27 @@
-<?php 
-
-
-print_r($_REQUEST);
+<?php
+include '../../app/config/config.php';
+include '../../app/config/conMysql.php';
 
 $number1 = $_GET['num1'];
-$number2 = $_GET['num1'];
-$number3 = $_GET['num1'];
-$number4 = $_GET['num1'];
+$number2 = $_GET['num2'];
+$number3 = $_GET['num3'];
+$number4 = $_GET['num4'];
+$email = $_GET['email'];
+$response = [];
 
+
+$verify_email = "select iduser from user where email = '{$email}'";
+$respUser = mysqli_query($con, $verify_email);
+$usuario = mysqli_fetch_array($respUser)[0];
+if (isset($usuario)) {
+    $codeInsert = intval($number1 . $number2 . $number3 . $number4);
+    $validaToken = "select code from tokenemail where iduser = $usuario and code = '{$codeInsert}'";
+    $respValidaToken = mysqli_query($con, $validaToken);
+    $tokenInsert = mysqli_fetch_array($respValidaToken);
+    if (isset($tokenInsert)) {
+        $response = ['status' => true, 'dados' => 'Altere a sua senha.'];
+    } else {
+        $response = ['status' => false, 'error' => 'Token invalida.'];
+    }
+}
+echo json_encode($response);
