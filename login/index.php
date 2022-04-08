d611313076c709e77f516101bb372143f8015d39
Gustavo Fernandes <gustavomarcelinofernandes25@gmail.com>
2022-04-08 08:45:49 -0300

    verify password

--- login/index.php
+++ login/index.php
@@ -2,18 +2,25 @@
 include '../app/config/config.php';
 include '../app/config/conMysql.php';
 $login = isset($_GET['usuario']) ? $_GET['usuario'] : null;
-$senha = isset($_GET['senha']) ? md5($_GET['senha']) : null;
+$senha = isset($_GET['senha']) ? $_GET['senha'] : null;
 
 function validaLogin($login, $senha, $con)
 {
     $return = [];
     $token = '';
-    $sqlVerificaUsuario = "select iduser, name, email, login, photo from user where login = '{$login}' and password = '{$senha}'";
+    $sqlVerificaUsuario = "select iduser, name, email, login, photo, password from user where login = '{$login}'";
+
     $respVerificaUsuario = mysqli_query($con, $sqlVerificaUsuario);
     $validaUsuario = mysqli_fetch_array($respVerificaUsuario);
+
     if (isset($validaUsuario[0])) {
-        $data = ['id' => $validaUsuario[0], 'nome' => $validaUsuario[1], 'email' => $validaUsuario[2], 'login' => $validaUsuario[3], 'token' => $token];
-        return ['status' => true, 'dados' => $data];
+
+        if (password_verify($senha, $validaUsuario[5])) {
+            $data = ['id' => $validaUsuario[0], 'nome' => $validaUsuario[1], 'email' => $validaUsuario[2], 'login' => $validaUsuario[3], 'token' => $token];
+            return ['status' => true, 'dados' => $data];
+        } else {
+            return ['status' => false, 'dados' => 'Senha incorreta.'];
+        }
     } else {
         $return = ['status' => false, 'error' => 'Usuário não encontrado.'];
     }
