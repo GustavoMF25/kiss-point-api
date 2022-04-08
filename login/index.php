313fb34ab707a6a5aace0089ae401863818e6409
Gustavo Fernandes <gustavomarcelinofernandes25@gmail.com>
2022-04-08 09:50:11 -0300

    Fix error

--- login/index.php
+++ login/index.php
@@ -27,7 +27,7 @@ function validaLogin($login, $senha, $con)
     return $return;
 }
 
-// print_r($_GET);
+print_r($_GET);
 if (isset($login) && isset($senha)) {
     include '../app/config/conMysql.php';
     $response = [];
