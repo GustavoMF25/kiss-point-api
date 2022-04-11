966a20d51c9b6a11fe780ff97b09d9e8ad575d86
Gustavo Fernandes <gustavomarcelinofernandes25@gmail.com>
2022-04-11 10:53:19 -0300

    create page forgetPassword/change

---
+++ login/forgetpassword/change.php
@@ -0,0 +1,13 @@
+<?php
+include '../../app/config/config.php';
+include '../../app/config/conMysql.php';
+
+
+$newPassword = $_POST['newPassword'];
+$idusuario = $_POST['idusuario'];
+
+if (isset($newPassword) && isset($newPassword)) {
+    
+}
+
+print_r($_POST);
