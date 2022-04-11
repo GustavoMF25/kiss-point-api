b540d8e26cacaf393977eb65291ea8fdf7118fc2
Gustavo Fernandes <gustavomarcelinofernandes25@gmail.com>
2022-04-11 11:52:08 -0300

    alter password

--- login/forgetpassword/change.php
+++ login/forgetpassword/change.php
@@ -3,11 +3,22 @@ include '../../app/config/config.php';
 include '../../app/config/conMysql.php';
 
 
-$newPassword = $_POST['newPassword'];
-$idusuario = $_POST['idusuario'];
+$newPassword = isset($_GET['newPassword']) ? $_GET['newPassword'] : NULL;
+$idusuario = isset($_GET['idusuario']) ? $_GET['idusuario'] : NULL;
 
-if (isset($newPassword) && isset($newPassword)) {
-    
+$response = [];
+function alterPassword($con, $newPassword, $idusuario)
+{
+    $hashed_password = password_hash($newPassword, PASSWORD_DEFAULT); //password encryption
+    $resp = [];
+    $sqlUpdatePassword = "UPDATE user SET password  = '{$hashed_password}' WHERE iduser = {$idusuario}";
+    if (mysqli_query($con, $sqlUpdatePassword)) {
+        $resp = ['status' => true, 'dados' => 'Senha alterada com sucesso'];
+    }
+    return $resp;
 }
 
-print_r($_REQUEST);
+if (isset($newPassword) && isset($newPassword)) {
+    $response = alterPassword($con, $newPassword, $idusuario);
+}
+echo json_encode($response);
