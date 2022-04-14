5279e2edeaf05b83299b69de44491fc5bd367224
Gustavo Marcelino Fernandes <47804327+GustavoMF25@users.noreply.github.com>
2022-04-13 21:46:06 -0300

    Update change.php

--- login/forgetpassword/change.php
+++ login/forgetpassword/change.php
@@ -2,7 +2,6 @@
 include '../../app/config/config.php';
 include '../../app/config/conMysql.php';
 
-print_r($_GET);
 $newPassword = isset($_GET['newPassword']) ? $_GET['newPassword'] : NULL;
 $idusuario = isset($_GET['idusuario']) ? $_GET['idusuario'] : NULL;
 $token = isset($_GET['token']) ? $_GET['token'] : NULL;
